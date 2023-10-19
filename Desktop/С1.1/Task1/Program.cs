05f1019f0b87474d4100c17728b124264eeec0c6
Elena Serebrennikova <eserebrennikovaa@mail.ru>
2023-10-19 16:31:59 +0300

    add Task1 progect

---
+++ Desktop/С1.1/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Введите число");
+string? strNumber = Console.ReadLine();
+int numberA = Convert.ToInt32(strNumber);
+int numberResult = numberA * numberA;
+
+Console.WriteLine("Квадрат от введенного Вами числа " + numberA + " будет " + numberResult);
